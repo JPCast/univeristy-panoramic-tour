7b1f9805370f1dc69b67c353b5a092f6f4b24b2a
Carlos Ribeiro <carlosfiliperibeiro@ua.pt>
2018-11-17 22:21:06 +0000

    Outra imagem para mapa

--- Example_28_Textures/WebGL_example_28.js
+++ Example_28_Textures/WebGL_example_28.js
@@ -236,8 +236,10 @@ function handleLoadedTexture(texture) {
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 	gl.bindTexture(gl.TEXTURE_2D, null);
 }
 
@@ -266,7 +268,7 @@ function initTexture() {
     webGLTexture3.image.onload = function () {
         handleLoadedTexture(webGLTexture3)
     }
-    webGLTexture3.image.src = "alameda.gif";
+    webGLTexture3.image.src = "universidade.jpg";
 }
 
 //----------------------------------------------------------------------------
